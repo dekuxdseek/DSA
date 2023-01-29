af3c0a7822235ff7e55c3602f0e80b13e90db8ac
Swastik Sharma <swastiksharmaaa09@gmail.com>
2023-01-29 23:37:10 +0530

    ll start

---
+++ LinkedList/uses.cpp
@@ -0,0 +1,24 @@
+/*
+    uses of linked list in real life :
+    1. used in image gallery (to toggle between images)
+    2. used to switch songs in music player
+    3. to go back and forth the previous and next tabs
+    4. implementing stacks and queues
+    5. hashmap and hashset: used in chaining (when collision occurs chaining is used in hashmap and hashsets)
+
+
+    definition of linked list:
+
+    struct Node{
+        int value;
+        Node* link;
+    }
+
+    links are there to access other nodes/other elements of this structure
+    //nodes are nothing but elements of this data structure
+
+
+
+    // representation of linked list in memory
+    // "new" keyword is used to allocate space in heap ( amount of space = )
+*/
